9aad179ac5379c259d8985af127fdb7a282d99a5
Robbie McKinstry <thesnowmancometh@gmail.com>
2024-05-11 18:10:12 -0400

    Toy Provider and Platform Definitions
    This commit adds some toy cloud provider and platform definitions.
    I'm playing around with some ideas for representing the types and
    interfaces. This is just a first pass at very vague, high level
    definitions. I wanted to see if I could keep everything object safe
    while allowing the types to interact with each other a bit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+trait Named {
+    fn name(&self) -> &str;
+}
+
+impl<T: StaticallyNamed> Named for T {
+    fn name(&self) -> &str {
+        T::NAME
+    }
+}
+
+trait StaticallyNamed {
+    const NAME: &'static str;
+}
+
+/// A `CloudProvider` represents a single cloud.
+trait CloudProvider: Named {
+    // must be able to create an instance so we can access metadata like name().
+    fn new() -> Self
+    where
+        Self: Sized;
+}
+
+impl StaticallyNamed for Aws {
+    const NAME: &'static str = "aws";
+}
+
+struct Aws;
+impl CloudProvider for Aws {
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        Self
+    }
+}
+
+/// A `Platform` is somewhere code can be deployed e.g. Kubernetes,
+/// a VM, or serverless
+trait Platform: Named {}
+
+struct Serverless;
+
+impl StaticallyNamed for Serverless {
+    const NAME: &'static str = "serverless";
+}
+
+impl Platform for Serverless {}
+
+/// An adaptor contains the set of instructions to deploy an app
+/// to a given `Platform` on a given `CloudProvider`.
+trait Adaptor {
+    /// each adaptor can self-describe the cloud for which it was built.
+    fn cloud(&self) -> &dyn CloudProvider;
+
+    /// each adaptor can self-describe the platform for which it was built.
+    fn platform(&self) -> &dyn Platform;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    use super::{Adaptor, CloudProvider, Platform};
+    use static_assertions::assert_obj_safe;
+
+    assert_obj_safe!(Adaptor);
+    assert_obj_safe!(CloudProvider);
+    assert_obj_safe!(Platform);
 }
