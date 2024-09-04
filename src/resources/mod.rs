dfb717dabf7d96693ca8f9d9ab7f941a307c8a24
Robbie McKinstry <thesnowmancometh@gmail.com>
2024-09-04 18:03:47 -0400

    Add static resource class definition

---
+++ src/resources/mod.rs
@@ -0,0 +1,13 @@
+//! Resource can be defined either statically or dynamically.
+//! A static resource is one whose definition is hard-coded in Rust.
+//! We pre-define all of the resource's CRUDL operations in Rust.
+//!
+//! In contrast, a dynamic resource is one that's loaded from WebAssembly.
+//! The type-checker inspects the Wasm Component's API to determine
+//! the resource's schema.
+//!
+//! At the time of writing, dynamic resources are not supported.
+
+pub use static_::ResourceClass as StaticResourceClass;
+
+mod static_;
