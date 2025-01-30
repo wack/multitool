9f194651f15ab93bb17a7e42f1d2293e6292cd2d
Robbie McKinstry <robbie@wack.run>
2025-01-30 13:59:11 -0500

    Delete files that are no longer needed for this version of the product.

--- src/state/backend/local/mod.rs
+++
@@ -1,8 +0,0 @@
-pub use backend::LocalBackend;
-pub use journal::LocalJournal;
-
-mod backend;
-mod entry;
-mod entry_type;
-mod errors;
-mod journal;
--- src/state/history.rs
+++
@@ -1,3 +0,0 @@
-/// A `RunHistory` succintly stores the changes that occurred
-/// during a run.
-pub struct RunHistory;
