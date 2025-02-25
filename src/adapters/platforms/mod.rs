9e5312c17361785d0c23da8ea1a2f4072d190bec
Robbie McKinstry <robbie@wack.run>
2025-02-25 12:02:17 -0500

    Add test to demonstrate running the platform subsystem.

--- src/adapters/platforms/mod.rs
+++ src/adapters/platforms/mod.rs
@@ -1,12 +1,14 @@
 use async_trait::async_trait;
 use miette::Result;
+use mockall::automock;
 
 pub use builder::PlatformBuilder;
 pub use lambda::LambdaPlatform;
 
-use crate::Shutdownable;
+use crate::{Shutdownable, subsystems::ShutdownResult};
 pub type BoxedPlatform = Box<dyn Platform + Send + Sync>;
 
+#[automock]
 #[async_trait]
 pub trait Platform: Shutdownable {
     /// Deploy the canary app. Do not assign it any traffic.
@@ -15,6 +17,13 @@ pub trait Platform: Shutdownable {
     async fn yank_canary(&mut self) -> Result<()>;
     /// Make the canary app the new baseline.
     async fn promote_deployment(&mut self) -> Result<()>;
+}
+
+#[async_trait]
+impl Shutdownable for MockPlatform {
+    async fn shutdown(&mut self) -> ShutdownResult {
+        Ok(())
+    }
 }
 
 mod builder;
--- src/subsystems/platform/mod.rs
+++ src/subsystems/platform/mod.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use mail::PlatformMail;
+use mail::{DeployParams, PlatformMail, PromoteParams, YankParams};
 use miette::{IntoDiagnostic, Report, Result};
 use tokio::{
     select,
-    sync::mpsc::{self, channel},
+    sync::mpsc::{self, Receiver, Sender, channel},
     task::JoinHandle,
 };
 use tokio_graceful_shutdown::{IntoSubsystem, SubsystemHandle};
@@ -17,7 +17,6 @@ pub const PLATFORM_SUBSYSTEM_NAME: &str = "platform";
 /// than picking a power of two.
 const PLATFORM_MAILBOX_SIZE: usize = 1 << 4;
 
-use super::{ShutdownResult, Shutdownable};
 use mail::PlatformHandle;
 
 mod mail;
@@ -25,76 +24,125 @@ mod mail;
 /// The PlatformSubsystem handles sychronizing access to the
 /// `[BoxedPlatform]` using message-passing and channels.
 pub struct PlatformSubsystem {
-    task_done: JoinHandle<ShutdownResult>,
     handle: PlatformHandle,
+    platform: BoxedPlatform,
+    mailbox: Receiver<PlatformMail>,
+    shutdown: Receiver<()>,
 }
 
 impl PlatformSubsystem {
-    pub fn new(mut platform: BoxedPlatform) -> Self {
-        // • Spawn a new task with the BoxedPlatform and the mailbox.
-        let (shutdown_trigger, mut shutdown_signal) = channel(1);
-        let (mail_outbox, mut mail_inbox) = mpsc::channel(PLATFORM_MAILBOX_SIZE);
-        let task_done = tokio::spawn(async move {
-            loop {
-                select! {
-                    _ = shutdown_signal.recv() => {
-                        return platform.shutdown().await;
-                    }
-                    mail = mail_inbox.recv() => {
-                        if let Some(mail) = mail {
-                            match mail {
-                                PlatformMail::DeployCanary(deploy_params) => {
-                                    let outbox = deploy_params.outbox;
-                                    let result = platform.deploy().await;
-                                    outbox.send(result).unwrap();
-                                }
-                                PlatformMail::YankCanary(rollback_params) => {
-                                    let outbox = rollback_params.outbox;
-                                    let result = platform.yank_canary().await;
-                                    outbox.send(result).unwrap();
-                                }
-                                PlatformMail::PromoteDeployment(promote_params) => {
-                                    let outbox = promote_params.outbox;
-                                    let result = platform.yank_canary().await;
-                                    outbox.send(result).unwrap();
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        });
+    pub fn new(platform: BoxedPlatform) -> Self {
+        //   • We need to give the outside world a way to shutdown
+        //     via the handle, and we don't yet have access to the
+        //     subsystem. We could also do this by blocking the `handle()`
+        //     method until we have the subsystem available, but that's trickier
+        //     and potentially more deadlock-prone.
+        let (shutdown_trigger, shutdown_signal) = channel(1);
+        let (mail_outbox, mailbox) = mpsc::channel(PLATFORM_MAILBOX_SIZE);
         let shutdown = Arc::new(shutdown_trigger);
         let handle = PlatformHandle::new(Arc::new(mail_outbox), shutdown);
-        Self { task_done, handle }
+        Self {
+            handle,
+            platform,
+            mailbox,
+            shutdown: shutdown_signal,
+        }
     }
 
     pub fn handle(&self) -> BoxedPlatform {
         Box::new(self.handle.clone())
+    }
+
+    async fn handle_deploy(&mut self, params: DeployParams) {
+        let outbox = params.outbox;
+        let result = self.platform.deploy().await;
+        outbox.send(result).unwrap();
+    }
+
+    async fn handle_yank(&mut self, params: YankParams) {
+        let outbox = params.outbox;
+        let result = self.platform.yank_canary().await;
+        outbox.send(result).unwrap();
+    }
+
+    async fn handle_promote(&mut self, params: PromoteParams) {
+        let outbox = params.outbox;
+        let result = self.platform.yank_canary().await;
+        outbox.send(result).unwrap();
     }
 }
 
 #[async_trait]
 impl IntoSubsystem<Report> for PlatformSubsystem {
     async fn run(mut self, subsys: SubsystemHandle) -> Result<()> {
-        subsys.on_shutdown_requested().await;
-        // Propagate the shutdown signal.
-        let shutdown_result = self.handle.shutdown().await;
-        // Wait for the thread to shutdown.
-        let task_result = self.task_done.await.into_diagnostic();
-        shutdown_result.and(task_result?)
+        // Process all messages in a loop, while listening for shutdown.
+        loop {
+            select! {
+                // Shutdown comes first so it has high priority.
+                _ = subsys.on_shutdown_requested() => {
+                    return self.platform.shutdown().await;
+                }
+                _ = self.shutdown.recv() => {
+                    subsys.request_shutdown();
+                }
+                mail = self.mailbox.recv() => {
+                    if let Some(mail) = mail {
+                        match mail {
+                            PlatformMail::DeployCanary(params) => self.handle_deploy(params).await,
+                            PlatformMail::YankCanary(params) => self.handle_yank(params).await,
+                            PlatformMail::PromoteDeployment(params) => self.handle_promote(params).await,
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
+    use crate::adapters::MockPlatform;
     use crate::{adapters::Platform, subsystems::platform::mail::PlatformHandle};
 
-    use super::PlatformSubsystem;
+    use super::{PLATFORM_SUBSYSTEM_NAME, PlatformSubsystem};
     use miette::Report;
+    use miette::Result;
     use static_assertions::assert_impl_all;
-    use tokio_graceful_shutdown::IntoSubsystem;
+    use tokio::join;
+    use tokio_graceful_shutdown::{IntoSubsystem, SubsystemBuilder, Toplevel};
 
     assert_impl_all!(PlatformSubsystem: IntoSubsystem<Report>);
     assert_impl_all!(PlatformHandle: Platform, Clone);
+
+    /// This test demonstrates how to use the PlatformSubsystem.
+    /// It shows how you can launch it and call it's handle to
+    /// perform actions, and shut it down manually.
+    #[tokio::test]
+    async fn use_platform_subsystem() -> Result<()> {
+        // • We construct a mock platform to provide to the subsystem.
+        let mut mock_platform = MockPlatform::new();
+        mock_platform.expect_yank_canary().returning(|| Ok(()));
+        let platform_subsys = PlatformSubsystem::new(Box::new(mock_platform));
+        // • We create a handle so we can shutdown the system later.
+        let mut handle = platform_subsys.handle();
+        // • Launch the system.
+        let system_fut = Toplevel::new(|s| async move {
+            s.start(SubsystemBuilder::new(
+                PLATFORM_SUBSYSTEM_NAME,
+                platform_subsys.into_subsystem(),
+            ));
+        })
+        .handle_shutdown_requests(Duration::from_millis(1000));
+        let join_handle = tokio::spawn(system_fut);
+        // • Yank the canary.
+        assert!(handle.yank_canary().await.is_ok());
+        // • Wait for shutdown.
+        let (res1, res2) = join!(handle.shutdown(), join_handle);
+        // • Check errors.
+        assert!(res1.is_ok());
+        assert!(res2.is_ok());
+        Ok(())
+    }
 }
