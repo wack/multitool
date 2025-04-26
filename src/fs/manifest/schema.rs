f6c653fa2b429bc98cb3fdf2fe5ea4a080276462
Robbie McKinstry <robbie@wack.run>
2025-04-26 15:39:10 -0400

    Update manifest file with proper fields.

--- src/fs/manifest/schema.rs
+++ src/fs/manifest/schema.rs
@@ -1,4 +1,3 @@
-use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 
 /// With the expectation that we will likely be making breaking changes,
@@ -8,96 +7,37 @@ use serde::{Deserialize, Serialize};
 /// changes during its genesis. Wrapping the manifest in a version enum
 /// permits easy upgrades and is simple to program around.
 #[derive(Clone, Deserialize, Serialize, PartialEq, Eq, Debug)]
-#[serde(tag = "schema")]
-#[serde(rename = "manifest")]
-pub enum Manifest {
-    #[serde(rename = "alpha")]
-    Alpha(ManifestAlpha),
+pub struct Manifest {
+    workspace: Option<String>,
+    application: Option<String>,
 }
 
 impl Manifest {
-    pub fn dependencies(&self) -> IndexMap<String, Dependency> {
-        match self {
-            Manifest::Alpha(manifest) => manifest.dependencies.dependencies.clone(),
-        }
+    pub fn workspace(&self) -> Option<&str> {
+        self.workspace.as_deref()
     }
 
-    pub fn account(&self) -> &str {
-        match self {
-            Manifest::Alpha(manifest) => &manifest.account,
-        }
+    pub fn application(&self) -> Option<&str> {
+        self.application.as_deref()
     }
-
-    pub fn pkg_name(&self) -> &str {
-        match self {
-            Manifest::Alpha(manifest) => &manifest.pkg_name,
-        }
-    }
-
-    pub fn version(&self) -> &str {
-        match self {
-            Manifest::Alpha(manifest) => &manifest.version,
-        }
-    }
-}
-
-/// The manifest format for the alpha release of Wack.
-#[derive(Clone, Deserialize, Serialize, PartialEq, Eq, Debug)]
-#[serde(rename_all = "kebab-case")]
-pub struct ManifestAlpha {
-    pub account: String,
-    pub pkg_name: String,
-    pub version: String,
-    pub dependencies: DependencySection,
-}
-
-/// An object where the keys are the dependency name and the values
-/// describe which version.
-#[derive(Clone, Deserialize, Serialize, PartialEq, Eq, Debug, Default)]
-pub struct DependencySection {
-    #[serde(flatten)]
-    pub dependencies: IndexMap<String, Dependency>,
-}
-
-/// Anticipating a struct variant (like Cargo), we use an enum here
-/// in case we need to attach additional metadata to describe the dependency.
-#[derive(Deserialize, Serialize, PartialEq, Eq, Debug, Clone)]
-#[serde(untagged)]
-pub enum Dependency {
-    /// A Semver string
-    Version(String),
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Dependency, DependencySection, Manifest, ManifestAlpha};
-    use indexmap::indexmap;
+    use super::Manifest;
     use pretty_assertions::assert_str_eq;
 
     #[test]
     fn parse_example1() {
-        const RAW_MANIFEST: &str = r#"schema = "alpha"
-account = "foobar"
-pkg-name = "bazfizz"
-version = "v1.0.0"
-
-[dependencies]
-foo = "v2.0.0"
-bar = "v3.4.5"
+        const RAW_MANIFEST: &str = r#"workspace = "wack"
+application = "multitool"
 "#;
         let observed: Manifest = toml::from_str(RAW_MANIFEST).expect("manifest not parsable");
 
-        let expected = Manifest::Alpha(ManifestAlpha {
-            account: "foobar".to_owned(),
-            pkg_name: "bazfizz".to_owned(),
-            version: "v1.0.0".to_owned(),
-            dependencies: DependencySection {
-                dependencies: indexmap! {
-                    String::from("foo") => Dependency::Version("v2.0.0".to_owned()),
-                    String::from("bar") => Dependency::Version("v3.4.5".to_owned()),
-                },
-            },
-        });
+        let expected = Manifest {
+            workspace: Some("wack".to_owned()),
+            application: Some("multitool".to_owned()),
+        };
         assert_eq!(expected, observed);
 
         // Convert it back to a string and compare.
