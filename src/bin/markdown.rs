aa0148379037c8724a80305d03f76312dd8c1c19
Eric Ghildyal <ericghildyal@gmail.com>
2025-06-04 00:00:57 -0400

    Temporarily remove markdown docs for CLI

--- src/bin/markdown.rs
+++
@@ -1,9 +0,0 @@
-use clap::{CommandFactory, Parser};
-use multitool::{Cli, Terminal};
-
-fn main() {
-    // Parse the args provided to this process, including
-    // commands and flags.
-    let markdown: String = clap_markdown::help_markdown::<Cli>();
-    println!("{markdown}");
-}
