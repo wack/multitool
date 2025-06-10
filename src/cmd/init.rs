b5565bb1ca43559e787833680bd70785ee51bacf
Robbie McKinstry <robbie@wack.run>
2025-06-10 16:02:47 -0400

    Skeleton outline of a function-based state machine.
    This commit introduces the general idea of a function-based
    state machine for asking & answering questions to fill in the
    manifest. This state machine makes it very easy to isolate
    the current context from the rest of the otherwise messy questioning
    logic.
    
    Currently, it allows you to pass a mutable Manifest reference around,
    letting each state edit the manifest as more information is gathered.
    Right now, there is only one implementation; a global implementation
    that shows we can define the StateMachine trait on all types.
    
    Next, we plan to narrow this down into function types (closures) that
    implement the necessary parameters. This will give us a pleasant
    syntactic sugar to define new states as functions.

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,6 +1,15 @@
+//! The `init` subcommand is implemented as a state machine
+//! that maniputates stack allocated data with each state.
+//! The input is a (potentially empty) manifest file. Each state
+//! asks a question of the user to fill in the manifest file,
+//! like 20 Questions. As you answer more and more questions, you
+//! navigate deeper and deeper into the manifest's structure,
+//! until you reach a leaf node. Each leaf node corresponds to a
+//! field in the manifest.
+
 use miette::Result;
 
-use crate::Terminal;
+use crate::{Terminal, manifest::Manifest};
 
 pub struct Init {
     terminal: Terminal,
@@ -12,7 +21,42 @@ impl Init {
     }
 
     pub fn dispatch(self) -> Result<()> {
-        println!("Hello, world!");
+        // Create a new manifest instance.
+        // TODO: In the future, we should load this manifest
+        // from filesystem. See the git branch `robbie/init`
+        // for the code.
+        let start: Box<dyn InitState<Ctx = Manifest>> = Box::new(());
+        let mut manifest = Manifest::default();
+        let mut state = Some(start);
+        while let Some(next) = state {
+            state = next.run(&mut manifest)?;
+        }
+
         Ok(())
     }
+}
+
+impl<T> InitState for T {
+    type Ctx = Manifest;
+
+    fn run(self, _: &mut Self::Ctx) -> Result<Option<Box<dyn InitState<Ctx = Self::Ctx>>>> {
+        println!("Running once.");
+        Ok(None)
+    }
+}
+
+trait InitState {
+    type Ctx;
+
+    fn run(self, _: &mut Self::Ctx) -> Result<Option<Box<dyn InitState<Ctx = Self::Ctx>>>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::manifest::Manifest;
+
+    use super::InitState;
+    use static_assertions::assert_obj_safe;
+
+    assert_obj_safe!(InitState<Ctx = Manifest>);
 }
