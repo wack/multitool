0496352dfd71470d8d55ad6ccd561d6ff5ef26c3
Robbie McKinstry <robbie@wack.run>
2025-06-11 14:00:22 -0400

    Skeleton outline of a function-based state machine. (#112)
    * Skeleton outline of a function-based state machine.
    
    This commit introduces the general idea of a function-based
    state machine for asking & answering questions to fill in the
    manifest. This state machine makes it very easy to isolate
    the current context from the rest of the otherwise messy questioning
    logic.
    
    Currently, it allows you to pass a mutable Manifest reference around,
    letting each state edit the manifest as more information is gathered.
    Right now, there is only one implementation; a global implementation
    that shows we can define the StateMachine trait on all types.
    
    Next, we plan to narrow this down into function types (closures) that
    implement the necessary parameters. This will give us a pleasant
    syntactic sugar to define new states as functions.
    
    * Implement a more sound interface type for the StateMachine.
    
    This commit chnages the state machine's definition to allow each
    of the states to hold onto context, like a mutable reference to
    the manifest file, for instance.
    
    * Borrow manifest correctly.
    
    This commit changes the borrowing restrictions on the manifest
    and terminal pointers so we can actually pass these values from
    one state to another, which previously would not have been allowed,
    and would have immediately broken once we added a second state.
    
    * Rename State Machine => InitStateMachine for greppability.

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,6 +1,16 @@
+//! The `init` subcommand is implemented as a state machine
+//! that maniputates stack allocated data with each state.
+//! The input is a (potentially empty) manifest file. Each state
+//! asks a question of the user to fill in the manifest file,
+//! like 20 Questions. As you answer more and more questions, you
+//! navigate deeper and deeper into the manifest's structure,
+//! until you reach a leaf node. Each leaf node corresponds to a
+//! field in the manifest.
+
+use bon::Builder;
 use miette::Result;
 
-use crate::Terminal;
+use crate::{Terminal, manifest::Manifest};
 
 pub struct Init {
     terminal: Terminal,
@@ -11,8 +21,52 @@ impl Init {
         Self { terminal }
     }
 
-    pub fn dispatch(self) -> Result<()> {
-        println!("Hello, world!");
+    pub fn dispatch(mut self) -> Result<()> {
+        // Create a new manifest instance.
+        // TODO: In the future, we should load this manifest
+        // from filesystem. See the git branch `robbie/init`
+        // for the code.
+        let mut manifest = Manifest::default();
+        // Build the start state, passing in the terminal and manifest.
+        let start: &mut dyn InitStateMachine = &mut Start::builder()
+            .manifest(&mut manifest)
+            .terminal(&mut self.terminal)
+            .build();
+        // Run the state machine to completion.
+        let mut state = Some(start);
+        while let Some(next) = state {
+            state = next.run()?;
+        }
+
         Ok(())
     }
+}
+
+#[derive(Builder)]
+struct Start<'a> {
+    manifest: &'a mut Manifest,
+    terminal: &'a mut Terminal,
+}
+
+impl InitStateMachine for Start<'_> {
+    fn run(&mut self) -> Result<Option<&mut dyn InitStateMachine>> {
+        println!("Running once.");
+        let next = Self::builder()
+            .manifest(&mut self.manifest)
+            .terminal(&mut self.terminal)
+            .build();
+        Ok(None)
+    }
+}
+
+trait InitStateMachine {
+    fn run(&mut self) -> Result<Option<&mut dyn InitStateMachine>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::InitStateMachine;
+    use static_assertions::assert_obj_safe;
+
+    assert_obj_safe!(InitStateMachine);
 }
